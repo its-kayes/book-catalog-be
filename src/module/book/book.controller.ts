0a3ff0bedef92a57b9e25da16847ab47ce4d3dc4
its-kayes <kayes.ek8@gmail.com>
2023-07-15 02:06:09 +0600

    details by id

--- src/module/book/book.controller.ts
+++ src/module/book/book.controller.ts
@@ -3,23 +3,62 @@ import { throwResponse } from "../../shared/throwResponse";
 import catchAsync from "../../util/catchAsync";
 import { bookService } from "./book.service";
 import AppError from "../../errors/AppError";
+import { Book } from "./book.model";
+import httpStatus from "http-status";
 
-const addBook = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const { title, author, genre, publication } = req.body;
+const addBook = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { title, author, genre, publication } = req.body;
 
-  if (!title || !author || !genre || !publication) {
-    next(new AppError("Please provide all the details", 400))
+    if (!title || !author || !genre || !publication) {
+      next(
+        new AppError("Please provide all the details", httpStatus.BAD_REQUEST)
+      );
+    }
+
+    const book = await bookService.saveBook(req.body);
+
+    if (!book) {
+      next(
+        new AppError("Something went wrong", httpStatus.INTERNAL_SERVER_ERROR)
+      );
+    }
+
+    return throwResponse(
+      res,
+      book,
+      httpStatus.OK,
+      "Book added successfully",
+      true
+    );
   }
+);
 
-  const book = await bookService.saveBook(req.body);
+const bookDetailsById = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
 
-  if(!book) {
-    next(new AppError("Something went wrong", 500))
+    if (!id) {
+      next(new AppError("Please provide book id", httpStatus.BAD_REQUEST));
+    }
+
+    const book = await Book.findById(id);
+
+    if (!book) {
+      next(new AppError("No record found", httpStatus.NOT_FOUND));
+    }
+
+    return throwResponse(
+      res,
+      book,
+      httpStatus.OK,
+      "Book details fetched successfully",
+      true
+    );
   }
-
-  return throwResponse(res, book, 200, "Book added successfully", true);
-});
+);
 
 export const bookController = {
   addBook,
+  bookDetailsById
 };
--- src/module/book/book.routes.ts
+++ src/module/book/book.routes.ts
@@ -4,6 +4,8 @@ import { isVerified } from "../../middlewares/isVerified";
 
 const router: Router = Router();
 
-router.post("/add", isVerified, bookController.addBook)
+router.post("/add", isVerified, bookController.addBook);
+router.get("/details/:id", bookController.bookDetailsById);
+// router
 
 export { router as BookRoutes}
